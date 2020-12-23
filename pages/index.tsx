0303107b39c2492565844a9a86d3837355dd9adf
MINETA Hiroki(m10i) <h-mineta@0nyx.net>
2020-12-23 23:12:39 +0900

    [clean]

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,6 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <title>Create Next TypeScript App</title>
-        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
@@ -52,14 +51,8 @@ export default function Home() {
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
           Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+          <img src="/0nyx_logo.svg" alt="0nyx Logo" className={styles.logo} />
       </footer>
     </div>
   )
